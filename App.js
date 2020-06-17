89d3980c0199fb5a457f3978c71b86c27786a8e0
anapog <ana.pol.gonzalez@gmail.com>
2020-06-17 19:33:28 +0200

    feat(): timer entry component created

--- App.js
+++ App.js
@@ -1,21 +1,52 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import TimerEntry from './components/TimerEntry';
+import CountDown from './components/CountDown';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			actualTimer: 'work',
+			workSeconds: 0,
+			breakSeconds: 0,
+			remainingSeconds: 0,
+		}
+	}
+
+	setSeconds = (seconds) => {
+		if (this.state.actualTimer === 'work') {
+			this.setState({workSeconds: seconds});
+		} else {
+			this.setState({breakSeconds: seconds});
+		}
+		this.setState({remainingSeconds: seconds});
+	}
+
+	render() {
+		return (
+			<View style={styles.container}>
+				<Text style={[styles.bigFontSize, styles.bold]}>
+					{this.state.actualTimer.toUpperCase()} TIMER
+				</Text>
+				<TimerEntry title='Work timer:' onChange={this.setSeconds}/>
+				<TimerEntry title='Break timer:' onChange={this.setSeconds}/>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	bigFontSize: {
+		fontSize: 42,
+	},
+	bold: {
+		fontWeight: 'bold',
+	}
 });
---
+++ components/TimerEntry.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import { StyleSheet, Text, TextInput, View } from 'react-native'
+
+export default class TimerEntry extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			minutes: 0,
+			seconds: 0,
+		}
+	}
+
+	setMinutes = async minutes => {
+		this.checkInputValidity(+minutes)
+		await this.setState({minutes: +minutes});
+		this.props.onChange(this.state.minutes*60 + this.state.seconds);
+	}
+
+	setSeconds = async seconds => {
+		this.checkInputValidity(+seconds)
+		await this.setState({seconds: +seconds});
+		this.props.onChange(this.state.minutes*60 + this.state.seconds);
+	}
+
+	checkInputValidity = (num) => {
+		if (!Number.isInteger(num)) {
+			alert('Please enter a valid number!');
+		}
+	};
+
+	render() {
+		return (	
+			<View style={styles.timerEntry}>
+				<Text style={styles.bold}>{this.props.title}</Text>
+				<TextInput 
+					style={styles.input}
+					defaultValue={this.state.minutes.toString()}
+					placeholder='min' 
+					keyboardType='numeric'
+					onChangeText={this.setMinutes}/>
+				<Text>minutes</Text>
+				<TextInput 
+					style={styles.input}
+					defaultValue={this.state.seconds.toString()}
+					placeholder='sec' 
+					keyboardType='numeric'
+					onChangeText={this.setSeconds}/>
+				<Text>seconds</Text>
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	timerEntry: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		padding: 10
+	},
+	bold: {
+		flex: 1,
+		minWidth: 60,
+		fontWeight: 'bold',
+	},
+	input: {
+		flex: 1,
+		minWidth: 40,
+		marginRight: 10,
+		marginLeft: 10,
+		borderBottomWidth: 1,
+	},
+});
